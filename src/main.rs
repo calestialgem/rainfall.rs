64f13e1a3c2e409c1554167dee77a5121b49dca0
Cem GEÇGEL <gecgelcem@outlook.com>
2022-10-30 15:00:58 +0300

    clean: Project

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-use rainfall::Error;
-use std::env;
-
-/// Entry to the Rainfall compiler.
 fn main() {
-    // Get the arguments after the first one, which would be the path to the
-    // executable.
-    let args: Vec<String> = env::args().collect();
-    if let Err(error) = run(&args[1..]) {
-        println!("{}", error);
-    }
-}
-
-/// Run the compiler with the given arguments.
-fn run(args: &[String]) -> Result<(), Error> {
-    // Check arguments.
-    if args.len() > 0 {
-        return Err(Error::ArgumentError(String::from("Too many arguments!")));
-    }
-    Ok(())
+    println!("Hello!");
 }
